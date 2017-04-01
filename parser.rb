ed122f2fce4316d258acf8f933d66f662cc2fd22
Ranjan Kumar <ranjankumar188@gmail.com>
2017-04-01 14:55:36 +0530

    Listing property

--- parser.rb
+++ parser.rb
@@ -3,63 +3,62 @@ require 'json'
 require 'pry'
 
 class ZilloParser
+  ZilloUrl = 'https://www.zillow.com'.freeze
+  def self.parse_home_details(url)
+    mechanize = Mechanize.new
+    page = mechanize.get(url)
+    result = {}
+    result[:address] = page.at('.zsg-content-header.addr > h1').text.strip
+    result[:area] = page.at('.zsg-content-header.addr > h3').text.strip
+    result[:description] = page.at('.notranslate.zsg-content-item').text.strip
+    page.css('div.hdp-facts.zsg-content-component.z-moreless > div.fact-group-container.zsg-content-component.top-facts').each do |x|
+      fact = x.css('h3').text
+      facts = []
+      x.css('ul').each do |ul|
+        facts << ul.css('li').children.select(&:text?).collect(&:text).reject(&:empty?)
+      end
+      result[fact.to_sym] = facts.flatten
+    end
 
-	def self.parse_home_details(url)
-		mechanize = Mechanize.new
-		page = mechanize.get(url)
-		result = {}
-		result[:address] = page.at('.zsg-content-header.addr > h1').text.strip
-		result[:area] = page.at('.zsg-content-header.addr > h3').text.strip
-		result[:description] = page.at('.notranslate.zsg-content-item').text.strip
-		page.css('div.hdp-facts.zsg-content-component.z-moreless > div.fact-group-container.zsg-content-component.top-facts').each do |x|
-		  fact = x.css('h3').text
-		  facts = []
-		  x.css('ul').each do |ul|
-		    facts << ul.css('li').children.select(&:text?).collect(&:text).reject(&:empty?)
-		  end
-		  result[fact.to_sym] = facts.flatten
-		end
+    page.css('.zest-content').each do |zc|
+      t = zc.at('.zest-title').children.first.text.strip
+      result[t] = {}
+      result[t][:value] = zc.at('.zest-value').text
+      result[t][:low_range] = zc.css('.zest-range-bar-low').text
+      result[t][:high_range] = zc.css('.zest-range-bar-high').text
+    end
+    puts JSON.pretty_generate(result)
+  end
 
-		page.css('.zest-content').each do |zc|
-		  t = zc.at('.zest-title').children.first.text.strip
-		  result[t] = {}
-		  result[t][:value] = zc.at('.zest-value').text
-		  result[t][:low_range] = zc.css('.zest-range-bar-low').text
-		  result[t][:high_range] = zc.css('.zest-range-bar-high').text
-		end
-		puts JSON.pretty_generate(result)
-	end
+  def self.get_property_list(address)
+    puts "Searching For Property in #{address}"
+    mechanize = Mechanize.new
+    page = mechanize.get(ZilloUrl)
+    search_page = page.form_with(id: 'formSearchBar') do |form|
+      search_field = form.field_with(id: 'citystatezip')
+      search_field.value = address
+    end.submit
+    parse_property_list(search_page)
+  end
 
-	def self.get_property_list(address)
-		url = 'https://www.zillow.com/'
-		mechanize = Mechanize.new
-		page = mechanize.get(url)
-    search_page = page.form_with(:id => 'formSearchBar') do |form|
-    	search_field = form.field_with(:id => 'citystatezip')
-    	search_field.value = address
-		end.submit
-		puts search_page.uri
-		puts search_page.title
-		parse_property_list(search_page)
-	end	
-
-	def self.parse_property_list(page)
-		rs = []
-		
-	  page.css('div.zsg-photo-card-content.zsg-aspect-ratio-content').each do |pg|
+  def self.parse_property_list(page)
+    puts "Search Page Details: #{page.title} #{page.uri}"
+    rs = []
+    page.css('div.zsg-photo-card-content.zsg-aspect-ratio-content').each do |p|
       attrs = {}
-      #puts pg.at('a').attributes.first.last.value
-      attrs[:url] = pg.css('a').first.attributes['href'].text
-      
-      pg.css('div.zsg-photo-card-caption').each do |x|
-        attrs[:type] = x.at('.zsg-photo-card-status').text
+      attrs[:url] = ZilloUrl + p.css('a').first.attributes['href'].text
+      p.css('div.zsg-photo-card-caption').each do |pd|
+        attrs[:type] = pd.at('h4.zsg-photo-card-spec > .zsg-photo-card-status').text
+        attrs[:value] = pd.css('p.zsg-photo-card-spec > .zsg-photo-card-price').text
+        attrs[:address] = pd.at('p.zsg-photo-card-spec > .zsg-photo-card-address').text
       end
       rs << attrs
     end
- 
-    #puts JSON.pretty_generate(rs)
-    #parse_home_details(url) 
-	end	
+    puts "\n\t\t\t\tListing Top #{rs.size} properties\n"
+    puts JSON.pretty_generate(rs)
+    puts "\n\t\t\t\tProperty Details: #{rs.first[:address]}\n"
+    parse_home_details(rs.first[:url])
+  end
 end
 address = 'Easton, PA 18042'
 puts ZilloParser.get_property_list(address)
